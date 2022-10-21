450a06b3125e5226327dd2b9c3de78cb9ce66642
Mário Oliveira <oiramlopes@hotmail.com>
2022-10-21 00:15:11 -0300

    Cria uma lista do tipo Double e add itens a lista

--- ExemploListJava/src/App.java
+++ ExemploListJava/src/App.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+        System.out.println("Crie uma lista e adcione as sete notas");
+        List<Double> notas = new ArrayList<>(); // Cria um arraylist de dobles usando generics
+        // Adicionar itens a lista
+        notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(7.0);
+        notas.add(0.0);
+        notas.add(3.6);
+
+        // Imprime na tela o conteudo de notas
+        System.out.println(notas);
 
     }
 }
