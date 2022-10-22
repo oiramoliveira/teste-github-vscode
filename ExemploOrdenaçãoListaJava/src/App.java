7833176dce4806ded954503c34928e06f062777a
Mário Oliveira <oiramlopes@hotmail.com>
2022-10-21 21:37:47 -0300

    Implementando a interface comparable

--- ExemploOrdenaçãoListaJava/src/App.java
+++ ExemploOrdenaçãoListaJava/src/App.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.print.attribute.standard.JobName;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        List<Gato> meusGatos = new ArrayList<>() {
+            {
+                add(new Gato("Jon", 18, "Preto"));
+                add(new Gato("Simba", 6, "Tigrado"));
+                add(new Gato("Jon", 12, "Amarelo"));
+            }
+        };
+
+        // Ordem de inserção na list
+        System.out.println(meusGatos);
+
+        // ordem aleatoria
+        Collections.shuffle(meusGatos);
+        System.out.println(meusGatos);
+
+        // Ordem natural po nome, utiliza o comparable na classe gato
+
     }
 }
